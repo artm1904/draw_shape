175c2579af53ecbb7ece142d62f075430c9e4bcd
artm1904 <artmsavchenko@yandex.ru>
2025-08-14 13:04:24 +0300

    Added new shape strategy to ShapeFactory

--- src/Figures/ShapeFactory.cpp
+++ src/Figures/ShapeFactory.cpp
@@ -1,7 +1,9 @@
 #include "ShapeFactory.h"
 
 #include "Strategy/CircleStrategy.h"
+#include "Strategy/LineStrategy.h"
 #include "Strategy/RectangleStrategy.h"
+#include "Strategy/TextStrategy.h"
 #include "Strategy/TriangleStrategy.h"
 
 ShapeFactory::ShapeFactory() {
@@ -23,9 +25,29 @@ ShapeFactory::ShapeFactory() {
 
     _StrategyFactories["triangle"] = [](std::istream& args) -> std::unique_ptr<IShapeStrategy> {
         double x1, y1, x2, y2, x3, y3;
-        (args >> x1 >> y1 >> x2 >> y2 >> x3 >> y3);
         if (args >> x1 >> y1 >> x2 >> y2 >> x3 >> y3) {
             return std::make_unique<TriangleStrategy>(x1, y1, x2, y2, x3, y3);
+        }
+        return nullptr;
+    };
+
+    _StrategyFactories["line"] = [](std::istream& args) -> std::unique_ptr<IShapeStrategy> {
+        double x1, y1, x2, y2;
+        if (args >> x1 >> y1 >> x2 >> y2) {
+            return std::make_unique<LineStrategy>(x1, y1, x2, y2);
+        }
+        return nullptr;
+    };
+
+    _StrategyFactories["text"] = [](std::istream& args) -> std::unique_ptr<IShapeStrategy> {
+        double x, y, font;
+        std::string text;
+        if (args >> x >> y >> font) {
+            // Пропускаем пробел после числа и читаем всю оставшуюся часть строки
+            std::getline(args >> std::ws, text);
+            if (!text.empty()) {
+                return std::make_unique<TextStrategy>(x, y, font, text);
+            }
         }
         return nullptr;
     };
