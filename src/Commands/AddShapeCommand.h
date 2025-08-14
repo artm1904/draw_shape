d7cc3ad98d439765c5e39d3664e5cf0fe4ead89b
artm1904 <artmsavchenko@yandex.ru>
2025-08-14 10:35:01 +0300

    Fix AddShapesCommand

--- src/Commands/AddShapeCommand.h
+++ src/Commands/AddShapeCommand.h
@@ -1,19 +1,28 @@
 #pragma once
 
-#include "../Canvas/ICanvas.h"
+#include <memory>
+#include <string>
+
+#include "../Canvas/Color.h"
+#include "../Figures/IShapeStrategy.h"
 #include "../Shape/Picture.h"
+#include "../Shape/Shape.h"
 #include "ICommand.h"
 
 class AddShapesCommand : public ICommand {
    public:
-    AddShapesCommand(const Picture& picture, gfx::ICanvas& canvas) : _Picture(picture), _Canvas(canvas) {}
+    AddShapesCommand(Picture& picture, std::string id, gfx::Color color,
+                     std::unique_ptr<IShapeStrategy> strategy)
+        : _Picture(picture),
+          _Id(std::move(id)),
+          _Color(color),
+          _Strategy(std::move(strategy)) {}
 
-    void Execute() override {
-        return;
-        //_Picture.AddShape();
-    }
+    void Execute() override { _Picture.AddShape(shapes::Shape(_Id, _Color, std::move(_Strategy))); }
 
    private:
-    const Picture& _Picture;
-    gfx::ICanvas& _Canvas;
+    Picture& _Picture;
+    std::string _Id;
+    gfx::Color _Color;
+    std::unique_ptr<IShapeStrategy> _Strategy;
 };
