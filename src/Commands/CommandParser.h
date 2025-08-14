ce210a99584312d2b5bb353b119502ed43114112
artm1904 <artmsavchenko@yandex.ru>
2025-08-14 10:36:08 +0300

    Add AddShape command into CommandParser

--- src/Commands/CommandParser.h
+++ src/Commands/CommandParser.h
@@ -6,6 +6,7 @@
 #include <sstream>
 
 #include "../Canvas/ICanvas.h"
+#include "../Figures/ShapeFactory.h"
 #include "AllCommands.h"
 #include "ICommand.h"
 
@@ -13,12 +14,33 @@ class CommandParser {
    public:
     CommandParser(Picture& picture, gfx::ICanvas& canvas) : _Picture(picture), _Canvas(canvas) {
         _Commands["List"] = [this](std::istream& input) {
-            return std::make_unique<ListCommand>(_Picture);
+            return std::make_unique<ListCommand>(this->_Picture);
         };
 
-        _Commands["AddShape"] = [this](std::istream& input) {
-            return std::make_unique<AddShapesCommand>(_Picture, _Canvas);
+        _Commands["AddShape"] = [this](std::istream& input) -> std::unique_ptr<ICommand> {
+            std::string id, colorStr, shapeType;
+            if (!(input >> id >> colorStr >> shapeType)) {
+                std::cerr << "Error: Not enough arguments for AddShape." << std::endl;
+                return nullptr;
+            }
+
+            try {
+                gfx::Color color = gfx::Color::FromString(colorStr);
+                auto strategy = _ShapeFactory.CreateShape(shapeType, input);
+
+                if (!strategy) {
+                    std::cerr << "Error: Invalid shape type or parameters for '" << shapeType
+                              << "'." << std::endl;
+                    return nullptr;
+                }
+                return std::make_unique<AddShapesCommand>(_Picture, std::move(id), color,
+                                                          std::move(strategy));
+            } catch (const std::invalid_argument& e) {
+                std::cerr << "Error parsing AddShape command: " << e.what() << std::endl;
+                return nullptr;
+            }
         };
+        
         _Commands["DrawPicture"] = [this](std::istream& input) {
             return std::make_unique<DrawPictureCommand>(_Picture, _Canvas);
         };
@@ -43,4 +65,5 @@ class CommandParser {
     Picture& _Picture;
     gfx::ICanvas& _Canvas;
     std::map<std::string, CommandFactory> _Commands;
+    ShapeFactory _ShapeFactory;
 };
